849b61f20a8f200fe64fb0ba16b928f5fe5893c8
t0ri <victoriarosemurray@gmail.com>
2019-02-04 10:54:32 -0800

    init

---
+++ src/Components/Navbar/Components/UserState/UserState.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserState() {
+    return (
+        <div>
+            <a href="/logout"><p>Log Out</p></a>
+        </div>
+    )
+}
+
+export default UserState
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
+import Logo from './Components/Logo/Logo.jsx'
+import Notification from './Components/Notification/Nofication.jsx'
+import UserState from './Components/UserState/UserState.jsx'
+import LogInPrompt from './Components/LogInPrompt/LogInPrompt.jsx'
+
 export default class Navbar extends React.Component {
+    checkUserState() {
+        if (!this.props.currentState.loggedIn) {
+            return <LogInPrompt />
+        } else {
+            return <UserState />
+        }
+    }
+    
     render() {
-        let userState = this.props.currentState.loggedIn ? 'ya' : 'na'
         return (
-            <div><p>{userState}</p></div>
+            <div id="navbar">
+                {this.checkUserState()}
+            </div>
         )
     }
   }
