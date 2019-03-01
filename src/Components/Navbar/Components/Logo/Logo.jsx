4f90813aa141c2d784545a6df44aa43883913aff
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-03-01 11:23:57 -0800

    added logo but unsure how to import

--- src/Components/Navbar/Components/Logo/Logo.jsx
+++ src/Components/Navbar/Components/Logo/Logo.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Link } from 'react-router-dom'
 import './logo.css'
+// import logo from '../public/images/squilla-logo.png';
 
 function Logo() {
   return (
-    <div id="navbar-logo" className="navbar-item">
-      <Link to="/"><img src="./images/logo.png" alt="Squilla" /></Link>
+    <div id="navbar-logo" className="navbar">
+      <Link to="/">
+        <img src="" alt="Squilla" />
+      </Link>
     </div>
-  )
+  );
 }
 
 export default Logo
