02bd312cfa4e56b05aa0f18b8c8a74fa4cd4a0ee
t0ri <victoriarosemurray@gmail.com>
2019-03-07 14:12:44 -0800

    styled upload page, redirects on submit

--- src/Routes/NewPost/NewPost.jsx
+++ src/Routes/NewPost/NewPost.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import axios from 'axios'
+import { BrowserRouter as Router, Route, Link, Redirect, BrowserHistory } from 'react-router-dom'
 
-import './NewPost.css'
+import './newpost.css'
 
 export default class NewPost extends React.Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      response: null,
+    }
+
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.responseSetState = this.responseSetState.bind(this)
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -16,20 +22,44 @@ export default class NewPost extends React.Component {
     const formData = new FormData(event.target)
 
     axios.post('/api/art', formData)
-      .then(res => console.log(res))
-      .catch(err => console.log(err.message))
+      .then((res) => this.responseSetState(res))
+  }
+
+  responseSetState(res) {
+    console.log(res)
+    this.setState({
+      response: res.status
+    })
+  }
+
+  submitRedirect() {
+    const { response } = this.state
+
+    if (response === 200) {
+      return <Redirect to="/" />
+    }
+
+    return null
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="upload-btn-wrapper">
-          <button className="btn">Upload a file</button>
+      <form id="new-post-form-container" onSubmit={this.handleSubmit}>
+        <div id="new-post-upload-btn-wrapper">
+          <button id="new-post-btn">Upload Art</button>
+          <input type="file" name="art" id="new-post-file-upload" />
         </div>
-        <input type="file" name="art" />
-        <input type="text" name="name" />
-        <input type="text" name="description" />
-        <button type="submit">Submit</button>
+        <label className="new-post-input-label">
+          Title
+          <input type="text" name="name" className="new-post-input-text" />
+        </label>
+        <label className="new-post-input-label">
+          Description
+          <textarea name="description" className="new-post-input-text" rows="4" cols="50" maxLength="3000" placeholder="Max character count: 3000"></textarea>
+        </label>
+        <button type="submit" id="new-post-submit">Submit</button>
+
+        {this.submitRedirect()}
       </form>
     )
   }
